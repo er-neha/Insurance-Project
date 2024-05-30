01c5c479698bc88d5f6adeece7cae1ff7fb05d19
Malusare306 <Malusare306@github.com>
2024-05-30 23:39:46 +0530

    added payment service

---
+++ PaymentService/src/main/java/com/example/paymentservice/model/PaymentMode.java
@@ -0,0 +1,9 @@
+package com.example.paymentservice.model;
+
+public enum PaymentMode {
+    CASH,
+    CREDIT_CARD,
+    DEBIT_CARD,
+    NET_BANKING,
+    UPI
+}
---
+++ PaymentService/src/main/java/com/example/paymentservice/service/PaymentService.java
@@ -0,0 +1,51 @@
+package com.example.paymentservice.service;
+
+import com.example.paymentservice.model.Payment;
+import com.example.paymentservice.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentService {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
+
+    public Payment getPaymentById(Long id) {
+        return paymentRepository.findById(id).orElse(null);
+    }
+
+    public Payment createPayment(Payment payment) {
+        return paymentRepository.save(payment);
+    }
+
+    public Payment updatePayment(Long id, Payment paymentDetails) {
+        Payment payment = paymentRepository.findById(id).orElse(null);
+
+        if (payment != null) {
+            payment.setBillId(paymentDetails.getBillId());
+            payment.setAmountPaid(paymentDetails.getAmountPaid());
+            payment.setUserFirstName(paymentDetails.getUserFirstName());
+            payment.setUserLastName(paymentDetails.getUserLastName());
+            payment.setAccountNumber(paymentDetails.getAccountNumber());
+            payment.setTransactionDate(paymentDetails.getTransactionDate());
+            payment.setModeOfPayment(paymentDetails.getModeOfPayment());
+            payment.setPaymentStatus(paymentDetails.getPaymentStatus());
+            payment.setPolicyId(paymentDetails.getPolicyId());
+            payment.setDriverId(paymentDetails.getDriverId());
+            return paymentRepository.save(payment);
+        } else {
+            return null;
+        }
+    }
+
+    public void deletePayment(Long id) {
+        paymentRepository.deleteById(id);
+    }
+}
